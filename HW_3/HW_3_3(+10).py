854aa281fd20dde185e991b77d4a9c5c89f66dd1
Harizzon <dmitriypodgora@gmail.com>
2020-02-28 13:15:41 +0200

    Added HW_3_3 hotfix_1

--- HW_3/HW_3_3(+10).py
+++ HW_3/HW_3_3(+10).py
@@ -1,6 +1,6 @@
 money = [500, 100, 10, 20, 1]
 money.reverse()
-given_bills = {500: '0', 100: "0",20: "0", 10: "0", 1: "0"}
+given_bills = {500: '0', 100: "0", 20: "0", 10: "0", 1: "0"}
 w_money = total_money = int(input())
 
 while w_money > 0:
@@ -10,6 +10,7 @@ while w_money > 0:
                 rez = w_money // x
                 if rez >= 10:
                     rez = 10
+                    given_bills[x] = 10
                 else:
                     rez = w_money // x
                 w_money -= x * rez
@@ -17,11 +18,13 @@ while w_money > 0:
             if (w_money // x) > 0:
                 rez = w_money // x
                 w_money -= rez * x
+                given_bills[x] = rez
 
 print(f"""
 We've given to the client this amount of money {total_money}
 500 bills: {given_bills[500]},
 100 bills: {given_bills[100]},
+20 bills:  {given_bills[20]},
 10 bills:  {given_bills[10]},
 1 bills:   {given_bills[1]}
 
